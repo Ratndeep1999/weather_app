2ea41abe177bf3d7188bbb900351df6dabd1e36b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-11 09:34:36 +0530

    splash_page.dart : splash page add

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext _) {
+    return Scaffold();
+  }
+}
