38a733984bfb723c05e8b3c22ff3c84df7443cc9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-11 09:52:00 +0530

    splash_page.dart : _moveToNextScreen() add to navigate homescreen and called this method inside initState

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -9,7 +10,22 @@ class SplashPage extends StatefulWidget {
 
 class SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    _moveToNextScreen();
+    super.initState();
+  }
+
+  /// navigate to next screen
+  _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => HomePage()),
+    );
+  }
+
+  @override
   Widget build(BuildContext _) {
-    return Scaffold();
+    return Scaffold(body: Center(child: Text("Weather Forcest App")));
   }
 }
