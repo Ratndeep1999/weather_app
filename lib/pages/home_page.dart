797faca40e27b838e50f2d09502ac26543dd4a84
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-11 10:27:43 +0530

    home_page.dart : AppGradientBackground class add and child pass

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/app_gradient_background.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,10 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(_) {
-    return Scaffold();
+    return Scaffold(body: AppGradientBackground(child: Column(
+      children: [
+
+      ],
+    )));
   }
 }
