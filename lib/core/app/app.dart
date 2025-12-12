f2971f10f2c68502d7642e468bfa33e8aba73ed8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-12 17:54:12 +0530

    app.dart : temporary remove splash page

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home_page.dart';
 import '../../pages/splash_page.dart';
 
 class App extends StatelessWidget {
@@ -12,7 +13,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
