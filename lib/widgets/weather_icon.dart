f381d3b4a08c1fdc2d0092f734e7285ecdc32f7b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-12 18:39:57 +0530

    weather_icon.dart : icon widget create to give responsive weather icon.

---
+++ lib/widgets/weather_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class WeatherIcon extends StatelessWidget {
+  const WeatherIcon({
+    super.key,
+    required this.height,
+    required this.width,
+  });
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      "assets/icons/sun_cloud.svg",
+      height: height * 0.225,
+      width: width * 0.45,
+    );
+  }
+}
