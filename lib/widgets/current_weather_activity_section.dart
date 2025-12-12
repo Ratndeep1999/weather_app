54208239c8c0ff3978f849aa61bc005daa24c89b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-12 21:49:52 +0530

    current_weather_activity_section.dart : replace Decorated box with custom background_decorated_box_widget by giving dynamic vertical and horizontal padding.

--- lib/widgets/current_weather_activity_section.dart
+++ lib/widgets/current_weather_activity_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/background_decorated_box_widget.dart';
 import 'package:weather_app/widgets/weather_activity_widget.dart';
 
 class CurrentWeatherActivitySection extends StatelessWidget {
@@ -13,40 +14,24 @@ class CurrentWeatherActivitySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: Color(0xFF0c3a8a),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: width * 0.0875,
-            vertical: height * 0.0125,
+    return BackgroundDecoratedBoxWidget(
+      horizontalPadding: width * 0.0875,
+      verticalPadding: height * 0.0125,
+      customWidget: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          /// rain
+          WeatherActivityWidget(icon: "assets/icons/rain.svg", label: "6%"),
+
+          /// humidity
+          WeatherActivityWidget(
+            icon: "assets/icons/humidity.svg",
+            label: "90%",
           ),
 
-          /// Activity and Details Section
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              /// rain
-              WeatherActivityWidget(icon: "assets/icons/rain.svg", label: "6%"),
-
-              /// humidity
-              WeatherActivityWidget(
-                icon: "assets/icons/humidity.svg",
-                label: "90%",
-              ),
-
-              /// wind
-              WeatherActivityWidget(
-                icon: "assets/icons/wind.svg",
-                label: "19km/h",
-              ),
-            ],
-          ),
-        ),
+          /// wind
+          WeatherActivityWidget(icon: "assets/icons/wind.svg", label: "19km/h"),
+        ],
       ),
     );
   }
