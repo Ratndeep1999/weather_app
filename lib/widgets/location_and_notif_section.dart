65b619e8eb03e3fa6f4b1665b5c2642b4d6eb6c3
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-12 22:09:36 +0530

    location_and_notif_section.dart : Row widget replace with two_items-widget and both widgets add

--- lib/widgets/location_and_notif_section.dart
+++ lib/widgets/location_and_notif_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:weather_app/widgets/two_items_widget.dart';
 
 class LocationAndNotifSection extends StatelessWidget {
   const LocationAndNotifSection({
@@ -13,54 +14,48 @@ class LocationAndNotifSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        /// select location section
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(
-              "assets/icons/location.svg",
-              semanticsLabel: "location",
-            ),
-            SizedBox(width: width * 0.02), //8
-            Text(
-              "Fortaleza",
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            SizedBox(width: width * 0.04), //16
-            SvgPicture.asset(
-              "assets/icons/down_arrow.svg",
-              semanticsLabel: "select location",
-            ),
-          ],
-        ),
-
-        /// notification section
-        Stack(
-          clipBehavior: Clip.none,
-          children: [
-            // no notify
-            SvgPicture.asset(
-              "assets/icons/no_notif.svg",
-              semanticsLabel: "no notification",
-            ),
-            // no notify
-            Visibility(
-              visible: true,
-              child: Positioned(
-                left: width * 0.03, // 12
-                bottom: height * 0.02, // 16
-                child: SvgPicture.asset(
-                  "assets/icons/yes_notif.svg",
-                  semanticsLabel: "notification",
-                ),
+    return TwoItemsWidget(
+      firstWidget: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            "assets/icons/location.svg",
+            semanticsLabel: "location",
+          ),
+          SizedBox(width: width * 0.02), //8
+          Text(
+            "Fortaleza",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          SizedBox(width: width * 0.04), //16
+          SvgPicture.asset(
+            "assets/icons/down_arrow.svg",
+            semanticsLabel: "select location",
+          ),
+        ],
+      ),
+      secondWidget: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          // no notify
+          SvgPicture.asset(
+            "assets/icons/no_notif.svg",
+            semanticsLabel: "no notification",
+          ),
+          // no notify
+          Visibility(
+            visible: true,
+            child: Positioned(
+              left: width * 0.03, // 12
+              bottom: height * 0.02, // 16
+              child: SvgPicture.asset(
+                "assets/icons/yes_notif.svg",
+                semanticsLabel: "notification",
               ),
             ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
