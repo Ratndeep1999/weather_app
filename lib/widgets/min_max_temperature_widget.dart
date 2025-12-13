b688aa81895299061b4ded26fce8e325bd9a646d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 15:21:53 +0530

    next_seven_days_forecast_widget.dart : both min and max Temperature parameters add

--- lib/widgets/min_max_temperature_widget.dart
+++ lib/widgets/min_max_temperature_widget.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class MinMaxTemperatureWidget extends StatelessWidget {
-  const MinMaxTemperatureWidget({super.key});
+  const MinMaxTemperatureWidget({
+    super.key,
+    required this.maxTemperature,
+    required this.minTemperature,
+  });
+
+  final int maxTemperature;
+  final int minTemperature;
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +17,7 @@ class MinMaxTemperatureWidget extends StatelessWidget {
         children: [
           /// Max Temperature
           TextSpan(
-            text: "13",
+            text: "$maxTemperature",
             style: TextStyle(
               color: Colors.white,
               fontSize: 16,
@@ -36,7 +43,7 @@ class MinMaxTemperatureWidget extends StatelessWidget {
 
           /// Min Temperature
           TextSpan(
-            text: "10",
+            text: "$minTemperature",
             style: TextStyle(
               color: Color(0xFF8499c2),
               fontSize: 16,
