30061e60209dbb3155a1478a7319c5ab033c7466
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 15:12:25 +0530

    min_max_temperature_widget.dart : min and max temperature widget refactor

---
+++ lib/widgets/min_max_temperature_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MinMaxTemperatureWidget extends StatelessWidget {
+  const MinMaxTemperatureWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          /// Max Temperature
+          TextSpan(
+            text: "13",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          WidgetSpan(
+            child: Transform.translate(
+              offset: Offset(0, -4),
+              child: Text(
+                "°C\t\t\t",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+
+          /// Min Temperature
+          TextSpan(
+            text: "10",
+            style: TextStyle(
+              color: Color(0xFF8499c2),
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          WidgetSpan(
+            child: Transform.translate(
+              offset: Offset(0, -4),
+              child: Text(
+                "°C",
+                style: TextStyle(
+                  color: Color(0xFF8499c2),
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
