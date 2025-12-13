9deca73ff391321be09038620c7d8bc186dd5669
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 10:39:12 +0530

    home_page.dart : dynamic height add before today_hourly_forecast_section.dart and refactored today_hourly_forecast_section.dart add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:weather_app/widgets/app_gradient_background.dart';
 import '../widgets/current_temperature_section.dart';
 import '../widgets/current_weather_activity_section.dart';
 import '../widgets/location_and_notif_section.dart';
+import '../widgets/today_hourly_forecast_section.dart';
 import '../widgets/weather_icon.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,6 +15,7 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   double get width => MediaQuery.of(context).size.width;
+
   double get height => MediaQuery.of(context).size.height;
 
   @override
@@ -23,7 +24,7 @@ class HomePageState extends State<HomePage> {
       body: AppGradientBackground(
         child: Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: width * 0.04, //16
+            horizontal: width * 0.08, //16
             vertical: height * 0.02, //16
           ),
           child: SingleChildScrollView(
@@ -31,18 +32,22 @@ class HomePageState extends State<HomePage> {
               children: [
                 /// Location and Notification Section
                 LocationAndNotifSection(width: width, height: height),
-                SizedBox(height: height * 0.0625),
+                SizedBox(height: height * 0.0625),//50
 
                 /// Weather Icon
                 WeatherIcon(height: height, width: width),
-                SizedBox(height: height * 0.025),
+                SizedBox(height: height * 0.025),// 20
 
                 /// Current Temperature Section
                 CurrentTemperatureSection(),
-                SizedBox(height: height * 0.05),
+                SizedBox(height: height * 0.05), //40
 
                 /// Current Weather Activity Section
                 CurrentWeatherActivitySection(width: width, height: height),
+                SizedBox(height: height * 0.025), //20
+
+                /// Today's Hourly Forecast Section
+                TodayHourlyForecastSection(width: width, height: height),
               ],
             ),
           ),
