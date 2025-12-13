d01544ad35a57d533c36ed534ad3682ea7a35c68
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 08:57:59 +0530

    current_weather_activity_section.dart : isBorder make false

--- lib/widgets/current_weather_activity_section.dart
+++ lib/widgets/current_weather_activity_section.dart
@@ -17,6 +17,7 @@ class CurrentWeatherActivitySection extends StatelessWidget {
     return BackgroundDecoratedBoxWidget(
       horizontalPadding: width * 0.05, //20
       verticalPadding: height * 0.0125, //10
+      isBorder: false,
       customWidget: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
