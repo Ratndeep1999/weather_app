725ad0ee9a96b7e8e080e3f5371c59c19d9337aa
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 11:27:32 +0530

    home_page.dart : horizontal padding change, refactored NextForecastSection() section add, and height add before NextForecastSection()

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:weather_app/widgets/app_gradient_background.dart';
 import '../widgets/current_temperature_section.dart';
 import '../widgets/current_weather_activity_section.dart';
 import '../widgets/location_and_notif_section.dart';
+import '../widgets/next_forecast_section.dart';
 import '../widgets/today_hourly_forecast_section.dart';
 import '../widgets/weather_icon.dart';
 
@@ -24,30 +25,30 @@ class HomePageState extends State<HomePage> {
       body: AppGradientBackground(
         child: Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: width * 0.08, //16
-            vertical: height * 0.02, //16
+            horizontal: 20, //32
+            vertical: 20, //32
           ),
           child: SingleChildScrollView(
             child: Column(
               children: [
                 /// Location and Notification Section
                 LocationAndNotifSection(width: width, height: height),
-                SizedBox(height: height * 0.0625),//50
-
+                SizedBox(height: height * 0.0625), //50
                 /// Weather Icon
                 WeatherIcon(height: height, width: width),
-                SizedBox(height: height * 0.025),// 20
-
+                SizedBox(height: height * 0.025), // 20
                 /// Current Temperature Section
                 CurrentTemperatureSection(),
                 SizedBox(height: height * 0.05), //40
-
                 /// Current Weather Activity Section
                 CurrentWeatherActivitySection(width: width, height: height),
                 SizedBox(height: height * 0.025), //20
-
                 /// Today's Hourly Forecast Section
                 TodayHourlyForecastSection(width: width, height: height),
+                SizedBox(height: height * 0.025), //20
+
+                /// Next Forecast  Section
+                NextForecastSection(width: width, height: height),
               ],
             ),
           ),
@@ -56,3 +57,4 @@ class HomePageState extends State<HomePage> {
     );
   }
 }
+
