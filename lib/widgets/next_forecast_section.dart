308e1d48f8a56b978bd64e6cfad7b313dde1b77c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-13 15:11:05 +0530

    next_forecast_section.dart : min and max temperature widget refactor

--- lib/widgets/next_forecast_section.dart
+++ lib/widgets/next_forecast_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'background_decorated_box_widget.dart';
 import 'next_forecast_first_row_widget.dart';
+import 'next_seven_days_forecast_widget.dart';
 
 class NextForecastSection extends StatelessWidget {
   const NextForecastSection({
@@ -17,7 +18,16 @@ class NextForecastSection extends StatelessWidget {
     return BackgroundDecoratedBoxWidget(
       horizontalPadding: width * 0.04, //16
       verticalPadding: height * 0.02, //16
-      customWidget: Column(children: [NextForecastFirstRowWidget()]),
+      customWidget: Column(
+        children: [
+          /// First Row
+          NextForecastFirstRowWidget(),
+          SizedBox(height: 20),
+
+          /// Next Seven Day's Forecast
+          NextSevenDaysForecastWidget(height: height, width: width),
+        ],
+      ),
     );
   }
 }
