67da5c8e48b336da7e3280d90a95b2e87df33937
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-15 15:41:46 +0530

    next_forecast_section.dart : DateTime.now get in today from home_page, _formatedWeekdays() - format weekday by getting addedDay and return weekday, _getWeekday() - return weekday in string.

--- lib/widgets/next_forecast_section.dart
+++ lib/widgets/next_forecast_section.dart
@@ -9,11 +9,41 @@ class NextForecastSection extends StatelessWidget {
     required this.isNight,
     required this.width,
     required this.height,
+    required this.today,
   });
 
   final bool isNight;
   final double width;
   final double height;
+  final DateTime today;
+
+  /// Return Weekday in String Format
+  String _formatedWeekdays(int addedDay) {
+    final weekday = today.add(Duration(days: addedDay)).weekday;
+    return _getWeekday(weekday);
+  }
+
+  /// Get the Weekday
+  _getWeekday(int weekday) {
+    switch (weekday) {
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      case 7:
+        return "Sunday";
+      default:
+        return ' ';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,50 +58,71 @@ class NextForecastSection extends StatelessWidget {
           SizedBox(height: 20),
 
           /// Next Seven Day's Forecast
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Monday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(0),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "10",
             maxTemperature: "13",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Tuesday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(1),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "15",
             maxTemperature: "12",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Wednesday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(2),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "13",
             maxTemperature: "10",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Thursday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(3),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "13",
             maxTemperature: "10",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Friday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(4),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "13",
             maxTemperature: "10",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Saturday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(5),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "13",
             maxTemperature: "10",
           ),
           SizedBox(height: 20),
-          NextSevenDaysForecastWidget(isNight: isNight, height: height, width: width,
-            forecastDay: "Sunday",
+          NextSevenDaysForecastWidget(
+            isNight: isNight,
+            height: height,
+            width: width,
+            forecastDay: _formatedWeekdays(6),
             weatherIcon: "assets/icons/sun_cloud.svg",
             minTemperature: "13",
             maxTemperature: "12",
