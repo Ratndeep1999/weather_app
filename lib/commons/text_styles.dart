c8e429a5a140ccc100d747896e17e7e323f8cb4d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-16 15:01:39 +0530

    text_styles.dart : Sf_Pro Font Family's Custom TextStyles Add.

---
+++ lib/commons/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+
+  // Sf_Pro_Semibold style
+  static TextStyle sfProSemibold = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  // Sf_Pro_Bold style
+  static TextStyle sfProBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  // Sf_Pro_Regular style
+  static TextStyle sfProRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+}
