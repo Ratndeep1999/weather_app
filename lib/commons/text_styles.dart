323474ab54d939105c747a4e69c196243d1ad5f8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-16 15:10:39 +0530

    text_styles.dart : Alegreya_Sans Font Family's Custom TextStyles Add.

--- lib/commons/text_styles.dart
+++ lib/commons/text_styles.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 
 class TextStyles {
-
   // Sf_Pro_Semibold style
   static TextStyle sfProSemibold = TextStyle(
     fontWeight: FontWeight.w600,
     color: Colors.white,
+    fontFamily: 'Sf_Pro',
   );
 
   // Sf_Pro_Bold style
   static TextStyle sfProBold = TextStyle(
     fontWeight: FontWeight.w700,
     color: Colors.white,
+    fontFamily: 'Sf_Pro',
   );
 
   // Sf_Pro_Regular style
   static TextStyle sfProRegular = TextStyle(
     fontWeight: FontWeight.w400,
     color: Colors.white,
+    fontFamily: 'Sf_Pro', // 'AlegreyaSans'
   );
 
+  // AlegreyaSansBold style
+  static TextStyle alegreyaSansBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+    fontFamily: 'AlegreyaSans',
+  );
+
+  // AlegreyaSansMedium style
+  static TextStyle alegreyaSansMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+    fontFamily: 'AlegreyaSans',
+  );
 }
