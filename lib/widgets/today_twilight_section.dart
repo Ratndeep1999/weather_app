1fb3aa3410da2517213993ca2f75072fb8b06635
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 16:36:10 +0530

    today_twilight_section.dart: left align widget refactor.

--- lib/widgets/today_twilight_section.dart
+++ lib/widgets/today_twilight_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../commons/text_styles.dart';
 import 'background_decorated_box_widget.dart';
 
 class TodayTwilightSection extends StatelessWidget {
@@ -20,12 +22,74 @@ class TodayTwilightSection extends StatelessWidget {
       isNight: isNight,
       customWidget: Column(
         children: [
-          Row(children: [Text("Sunrise")]),
-          Row(children: [Text("Moonrise")]),
+          /// Sunrise and Sunset
+          Row(
+            children: [
+              /// Left Side Widget
+              LeftAlignTwilightWidget(height: height, width: width),
+              Spacer(),
+
+              /// Right Side Widget
+              Column(
+                children: [
+                  /// Text then Icon
+                  Row(
+                    children: [
+                      Text("Sunset", style: TextStyles.alegreyaSansBold),
+                      SizedBox(width: width * 0.01),
+                      SvgPicture.asset(
+                        "assets/icons/sunset.svg",
+                        height: height * 0.04, // 40
+                        width: width * 0.2, // 40
+                      ),
+                    ],
+                  ),
+                  // Timing
+                  Text("06 pm", style: TextStyles.alegreyaSansBold),
+                ],
+              ),
+            ],
+          ),
+
+          /// Moonrise and Moonset
+          // Row(children: [Text("Moonrise")]),
         ],
       ),
       horizontalPadding: width * 0.05,
       verticalPadding: height * 0.0125,
+    );
+  }
+}
+
+class LeftAlignTwilightWidget extends StatelessWidget {
+  const LeftAlignTwilightWidget({
+    super.key,
+    required this.height,
+    required this.width,
+  });
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Icon then Text
+        Row(
+          children: [
+            SvgPicture.asset(
+              "assets/icons/sunrise.svg",
+              height: height * 0.04, // 40
+              width: width * 0.2, // 40
+            ),
+            SizedBox(width: width * 0.01),
+            Text("Sunrise", style: TextStyles.alegreyaSansBold),
+          ],
+        ),
+        // Timing
+        Text("06 am", style: TextStyles.alegreyaSansBold),
+      ],
     );
   }
 }
