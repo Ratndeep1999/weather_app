349f5d37524f42aaf7d9b0c0c1f8794c39b6fa0d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 21:22:57 +0530

    right_align_twilight_widget.dart: height: height * 0.04, // 32 comment add

--- lib/widgets/right_align_twilight_widget.dart
+++ lib/widgets/right_align_twilight_widget.dart
@@ -32,7 +32,7 @@ class RightAlignTwilightWidget extends StatelessWidget {
             /// Icon
             SvgPicture.asset(
               icon,
-              height: height * 0.04, // 40
+              height: height * 0.04, // 32
               width: width * 0.2, // 40
             ),
           ],
