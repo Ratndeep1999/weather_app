1be537b6ad382cf571895e6730c184542dd11e8e
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 17:05:12 +0530

    today_twilight_section.dart: LeftAlignTwilightWidget() all properties values add, RightAlignTwilightWidget() all properties values add.

--- lib/widgets/today_twilight_section.dart
+++ lib/widgets/today_twilight_section.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../commons/text_styles.dart';
+import 'package:weather_app/widgets/right_align_twilight_widget.dart';
 import 'background_decorated_box_widget.dart';
+import 'left_align_twilight_widget.dart';
 
 class TodayTwilightSection extends StatelessWidget {
   const TodayTwilightSection({
@@ -20,92 +19,58 @@ class TodayTwilightSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return BackgroundDecoratedBoxWidget(
       isNight: isNight,
-      customWidget: Column(
-        children: [
-          /// Sunrise and Sunset
-          Row(
-            children: [
-              /// Left Side Widget
-              LeftAlignTwilightWidget(height: height, width: width),
-              Spacer(),
-
-              /// Right Side Widget
-              RightAlignTwilightWidget(width: width, height: height),
-            ],
-          ),
-
-          /// Moonrise and Moonset
-          // Row(children: [Text("Moonrise")]),
-        ],
-      ),
       horizontalPadding: width * 0.05,
       verticalPadding: height * 0.0125,
-    );
-  }
-}
+      customWidget: Column(
+        children: [
+          Row(
+            children: [
+              /// Sunrise
+              LeftAlignTwilightWidget(
+                height: height,
+                width: width,
+                icon: "assets/icons/sunrise.svg",
+                label: "Sunrise",
+                actionTime: "06 am",
+              ),
+              Spacer(),
 
-class RightAlignTwilightWidget extends StatelessWidget {
-  const RightAlignTwilightWidget({
-    super.key,
-    required this.width,
-    required this.height,
-  });
+              /// Sunset
+              RightAlignTwilightWidget(
+                width: width,
+                height: height,
+                label: "Sunset",
+                icon: "assets/icons/sunset.svg",
+                actionTime: "06 pm",
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
 
-  final double width;
-  final double height;
+          Row(
+            children: [
+              /// Moonrise
+              LeftAlignTwilightWidget(
+                height: height,
+                width: width,
+                icon: "assets/icons/sunrise.svg",
+                label: "Moonrise",
+                actionTime: "07 pm",
+              ),
+              Spacer(),
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        /// Text then Icon
-        Row(
-          children: [
-            Text("Sunset", style: TextStyles.alegreyaSansBold),
-            SizedBox(width: width * 0.01),
-            SvgPicture.asset(
-              "assets/icons/sunset.svg",
-              height: height * 0.04, // 40
-              width: width * 0.2, // 40
-            ),
-          ],
-        ),
-        // Timing
-        Text("06 pm", style: TextStyles.alegreyaSansBold),
-      ],
-    );
-  }
-}
-
-class LeftAlignTwilightWidget extends StatelessWidget {
-  const LeftAlignTwilightWidget({
-    super.key,
-    required this.height,
-    required this.width,
-  });
-
-  final double height;
-  final double width;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        /// Icon then Text
-        Row(
-          children: [
-            SvgPicture.asset(
-              "assets/icons/sunrise.svg",
-              height: height * 0.04, // 40
-              width: width * 0.2, // 40
-            ),
-            SizedBox(width: width * 0.01),
-            Text("Sunrise", style: TextStyles.alegreyaSansBold),
-          ],
-        ),
-        // Timing
-        Text("06 am", style: TextStyles.alegreyaSansBold),
-      ],
+              /// Moonset
+              RightAlignTwilightWidget(
+                width: width,
+                height: height,
+                label: "Moonset",
+                icon: "assets/icons/sunset.svg",
+                actionTime: "05 am",
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
