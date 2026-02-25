bd109419e3f5b098b397b8409b19c5c506e1e099
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 21:55:38 +0530

    location_page.dart: final bool isNight, final double width, final double height, initState() and didChangeDependencies() add.

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/app_gradient_background.dart';
 
 class LocationPage extends StatefulWidget {
-  const LocationPage({super.key});
+  const LocationPage({
+    super.key,
+    required this.isNight,
+    required this.width,
+    required this.height,
+  });
+
+  final bool isNight;
+  final double width;
+  final double height;
 
   @override
   State<LocationPage> createState() => LocationPageState();
 }
 
 class LocationPageState extends State<LocationPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: AppGradientBackground(
+        isNight: widget.isNight,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.05, //20
+            vertical: height * 0.025, //20
+          ),
+          child: Column(children: []),
+        ),
+      ),
+    );
   }
 }
