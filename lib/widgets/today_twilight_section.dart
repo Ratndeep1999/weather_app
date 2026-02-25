e81ec1db45762182350832389dff9e77df2357a7
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 16:36:46 +0530

    today_twilight_section.dart: right align widget refactor.

--- lib/widgets/today_twilight_section.dart
+++ lib/widgets/today_twilight_section.dart
@@ -30,24 +30,7 @@ class TodayTwilightSection extends StatelessWidget {
               Spacer(),
 
               /// Right Side Widget
-              Column(
-                children: [
-                  /// Text then Icon
-                  Row(
-                    children: [
-                      Text("Sunset", style: TextStyles.alegreyaSansBold),
-                      SizedBox(width: width * 0.01),
-                      SvgPicture.asset(
-                        "assets/icons/sunset.svg",
-                        height: height * 0.04, // 40
-                        width: width * 0.2, // 40
-                      ),
-                    ],
-                  ),
-                  // Timing
-                  Text("06 pm", style: TextStyles.alegreyaSansBold),
-                ],
-              ),
+              RightAlignTwilightWidget(width: width, height: height),
             ],
           ),
 
@@ -57,6 +40,39 @@ class TodayTwilightSection extends StatelessWidget {
       ),
       horizontalPadding: width * 0.05,
       verticalPadding: height * 0.0125,
+    );
+  }
+}
+
+class RightAlignTwilightWidget extends StatelessWidget {
+  const RightAlignTwilightWidget({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Text then Icon
+        Row(
+          children: [
+            Text("Sunset", style: TextStyles.alegreyaSansBold),
+            SizedBox(width: width * 0.01),
+            SvgPicture.asset(
+              "assets/icons/sunset.svg",
+              height: height * 0.04, // 40
+              width: width * 0.2, // 40
+            ),
+          ],
+        ),
+        // Timing
+        Text("06 pm", style: TextStyles.alegreyaSansBold),
+      ],
     );
   }
 }
