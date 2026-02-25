3c5eb34690b76b6404d78086d774170e40a4b8e1
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-25 21:31:10 +0530

    location_page.dart: stateful widget add.

---
+++ lib/pages/location_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LocationPage extends StatefulWidget {
+  const LocationPage({super.key});
+
+  @override
+  State<LocationPage> createState() => LocationPageState();
+}
+
+class LocationPageState extends State<LocationPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
