2c09f5afa199dfa91d5593262c896c9523301524
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-26 19:14:18 +0530

    build.gradle.kts: compileSdk = 35 add

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.ratndeep.weather_app.weather_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
