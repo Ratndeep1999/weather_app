44270bac9c4df91f7d476ac5804e7a06d2d241bb
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-26 14:13:04 +0530

    location_page.dart: current location widget add, latitude and longitude widget add, submit button add.

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/commons/text_styles.dart';
 import 'package:weather_app/widgets/app_gradient_background.dart';
+import 'package:weather_app/widgets/background_decorated_box_widget.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({
@@ -42,7 +44,67 @@ class LocationPageState extends State<LocationPage> {
             horizontal: width * 0.05, //20
             vertical: height * 0.025, //20
           ),
-          child: Column(children: []),
+          child: Column(
+            children: [
+              /// Location Search Bar
+              /// Select Current Location
+              InkWell(
+                onTap: ()=> debugPrint("Current Location got"),
+                child: BackgroundDecoratedBoxWidget(
+                  isNight: widget.isNight,
+                  horizontalPadding: width * 0.05,
+                  verticalPadding: height * 0.0125,
+                  customWidget: Row(
+                    children: [
+                      /// Icon
+                      Icon(Icons.location_on, color: Colors.white),
+                      SizedBox(width: 20),
+
+                      /// Text
+                      Text(
+                        "Current Location",
+                        style: TextStyles.alegreyaSansBold,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              /// Current Location Lat and Long.
+              BackgroundDecoratedBoxWidget(
+                isNight: widget.isNight,
+                horizontalPadding: width * 0.05,
+                verticalPadding: height * 0.0125,
+                customWidget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /// Latitude
+                    Text("Latitude : 0.0", style: TextStyles.alegreyaSansBold),
+
+                    /// Longitude
+                    Text("Longitude : 0.0", style: TextStyles.alegreyaSansBold),
+                  ],
+                ),
+              ),
+
+              Spacer(),
+
+              /// Submit Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      widget.isNight ? Color(0xFF0c3a8a) : Color(0xFF278dc4),
+                    ),
+                  ),
+                  child: Text("Submit", style: TextStyles.alegreyaSansBold),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
