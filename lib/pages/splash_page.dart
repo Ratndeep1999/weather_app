af3242d0f66c737cda9c50964213c8a43c359238
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-26 16:18:22 +0530

    splash_page.dart: final DateTime nowDT = DateTime.now(); add, _getDayOrNight() call in initState(), and nowDT: nowDT, isNight: _getDayOrNight() parameters pass through HomePage().

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/background_decorated_box_widget.dart';
 import 'home_page.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,8 +10,11 @@ class SplashPage extends StatefulWidget {
 }
 
 class SplashPageState extends State<SplashPage> {
+  final DateTime nowDT = DateTime.now();
+
   @override
   void initState() {
+    _getDayOrNight();
     _goToNextPage();
     super.initState();
   }
@@ -20,13 +24,28 @@ class SplashPageState extends State<SplashPage> {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => HomePage()),
+        MaterialPageRoute(
+          builder: (_) => HomePage(nowDT: nowDT, isNight: _getDayOrNight()),
+        ),
       );
     });
   }
 
+  /// Method to get Night or Not
+  _getDayOrNight() {
+    final bool isNight = nowDT.hour > 18 || nowDT.hour < 6;
+    return isNight;
+  }
+
   @override
   Widget build(BuildContext _) {
-    return Scaffold(body: Center(child: Text("Weather Forcest App")));
+    return Scaffold(
+      body: BackgroundDecoratedBoxWidget(
+        isNight: _getDayOrNight(),
+        horizontalPadding: 16,
+        verticalPadding: 16,
+        customWidget: Center(child: Text("Weather Forcest App")),
+      ),
+    );
   }
 }
