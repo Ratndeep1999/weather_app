2d5ae8a1f00713f71e7ee327f245aee61357597f
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-26 15:08:34 +0530

    current_location_widget.dart: refactored current location widget add here.

---
+++ lib/widgets/current_location_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/commons/text_styles.dart';
+import 'background_decorated_box_widget.dart';
+
+class CurrentLocationWidget extends StatelessWidget {
+  const CurrentLocationWidget({
+    super.key,
+    required this.isNight,
+    required this.width,
+    required this.height,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+  final bool isNight;
+  final double height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: BackgroundDecoratedBoxWidget(
+        isNight: isNight,
+        horizontalPadding: width * 0.05,
+        verticalPadding: height * 0.0125,
+        customWidget: Row(
+          children: [
+            /// Icon
+            Icon(Icons.location_on, color: Colors.white),
+            SizedBox(width: 20),
+
+            /// Text
+            Text("Current Location", style: TextStyles.alegreyaSansBold),
+          ],
+        ),
+      ),
+    );
+  }
+}
