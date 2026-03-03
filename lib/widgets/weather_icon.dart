ae397661c6e44ae10db54e96940da9f876f19e5d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-03 10:02:58 +0530

    weather_icon.dart: weather status, server error status and location error status icons add.

--- lib/widgets/weather_icon.dart
+++ lib/widgets/weather_icon.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class WeatherIcon extends StatelessWidget {
   const WeatherIcon({
     super.key,
     required this.height,
     required this.width,
+    this.icon,
   });
 
+  final String? icon;
   final double height;
   final double width;
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      "assets/icons/sun_cloud.svg",
-      height: height * 0.225,
-      width: width * 0.45,
-      semanticsLabel: "Sunny",
-    );
+    return icon != null
+        /// Weather status icon
+        ? Image.network(
+            "http:$icon",
+            height: height * 0.225,
+            width: width * 0.45,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              /// Server error status icon
+              return Image.asset(
+                "assets/icons/server_error.png",
+                height: height * 0.225,
+                width: width * 0.45,
+              );
+            },
+          )
+        /// Location error status if icon is null
+        : Image.asset(
+            "assets/icons/location_error.png",
+            height: height * 0.225,
+            width: width * 0.45,
+          );
+
+    // SvgPicture.asset(
+    // "assets/icons/sun_cloud.svg",
+    // height: height * 0.225,
+    // width: width * 0.45,
+    // semanticsLabel: "Sunny",
+    // );
   }
 }
