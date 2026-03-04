50218024cfa6e5a2e017dd716021c85a31ab963d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-04 09:50:59 +0530

    current_temperature_section.dart: Documentation add, temp, currTemp, todayTemp, maxTemp and minTemp add indide build().

--- lib/widgets/current_temperature_section.dart
+++ lib/widgets/current_temperature_section.dart
@@ -9,33 +9,28 @@ class CurrentTemperatureSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String currentTepmC = weatherModel?.current?.tempC != null
-        ? "${weatherModel?.current?.tempC?.toStringAsFixed(0)}°"
-        : "--";
-    final String? max =
-        weatherModel?.forecast?.forecastday?[0].day?.maxtempC?.toStringAsFixed(
-          0,
-        ) ??
-        "--";
-    final String? min =
-        weatherModel?.forecast?.forecastday?[0].day?.mintempC?.toStringAsFixed(
-          0,
-        ) ??
-        "--";
+    /// Get Current Temperature
+    final double? temp = weatherModel?.current?.tempC;
+    final String currTemp = temp != null ? "${temp.toStringAsFixed(0)}°" : "--";
+
+    /// Get Today Temperature's
+    final Day? todayTemp = weatherModel?.forecast?.forecastday?[0].day;
+    final String maxTemp = todayTemp?.maxtempC?.toStringAsFixed(0) ?? "--";
+    final String minTemp = todayTemp?.mintempC?.toStringAsFixed(0) ?? "--";
 
     return RichText(
       textAlign: TextAlign.center,
       text: TextSpan(
         /// Current Temperature
-        text: currentTepmC,
+        text: currTemp,
         style: TextStyles.sfProSemibold.copyWith(fontSize: 80.0),
         semanticsLabel: "Temperature",
         children: [
           TextSpan(text: "\nPrecipitations\n", style: TextStyles.sfProRegular),
 
-          /// Min and Max Temperature
+          /// Forecast Day Min and Max Temperature
           TextSpan(
-            text: "Max.: $max°   Min.: $min°",
+            text: "Max.: $maxTemp°   Min.: $minTemp°",
             style: TextStyles.sfProRegular,
           ),
         ],
