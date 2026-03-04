aaa3c220a89f182d85bf369b04a5563fafb286f6
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-04 21:49:04 +0530

    hourly_weather_report_widget.dart: padding() widget remove.

--- lib/widgets/hourly_weather_report_widget.dart
+++ lib/widgets/hourly_weather_report_widget.dart
@@ -26,29 +26,26 @@ class HourlyWeatherReportWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     /// Hourly Report Widget
-    return Padding(
-      padding: EdgeInsets.only(right: width * 0.032), // 12
-      child: BackgroundDecoratedBoxWidget(
-        isNight: isNight,
-        boxWidth: width * 0.18, // 70
-        horizontalPadding: width * 0.0,
-        verticalPadding: height * 0.02, // 16
-        backgroundColor: isCurrentHours
-            ? (isNight ? Color(0xFF12428d) : Color(0xFF2d92cc))
-            : null,
-        isBorder: isCurrentHours,
-        customWidget: Column(
-          children: [
-            /// Temperature
-            Text("$temperature°C", style: TextStyles.sfProRegular),
-            SizedBox(height: height * 0.02), // 16
-            /// Weather Report Icon
-            WeatherIcon(iconSize: height * 0.055, icon: icon),
-            SizedBox(height: height * 0.02), // 16
-            /// Hours
-            Text(hours, style: TextStyles.sfProRegular),
-          ],
-        ),
+    return BackgroundDecoratedBoxWidget(
+      isNight: isNight,
+      boxWidth: width * 0.18, // 70
+      horizontalPadding: width * 0.0,
+      verticalPadding: height * 0.02, // 16
+      backgroundColor: isCurrentHours
+          ? (isNight ? Color(0xFF12428d) : Color(0xFF2d92cc))
+          : null,
+      isBorder: isCurrentHours,
+      customWidget: Column(
+        children: [
+          /// Temperature
+          Text("$temperature°C", style: TextStyles.sfProRegular),
+          SizedBox(height: height * 0.02), // 16
+          /// Weather Report Icon
+          WeatherIcon(iconSize: height * 0.055, icon: icon),
+          SizedBox(height: height * 0.02), // 16
+          /// Hours
+          Text(hours, style: TextStyles.sfProRegular),
+        ],
       ),
     );
   }
