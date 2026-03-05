e7635fe89840bc33fca09ffe9da5a44c4b9a827d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-05 19:20:18 +0530

    location_service.dart: code add inside checkPermission().

--- lib/Services/location_service.dart
+++ lib/Services/location_service.dart
@@ -1,7 +1,33 @@
-class LocationService {
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/core/Exceptions/location_exception.dart';
 
+class LocationService {
   /// Check location permission and service status
-  void checkPermission() {}
+  Future<void> checkPermission() async {
+    bool isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    /// Device Location Status
+    if (!isLocationEnabled) throw AppLocationServiceDisabledException();
+
+    /// Get Permission Status
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    /// If denied
+    if (permission == LocationPermission.denied) {
+      /// Store requestPermission()
+      permission = await Geolocator.requestPermission();
+
+      /// Ask Permission and Store Status
+      if (permission == LocationPermission.denied) {
+        throw AppLocationPermissionDeniedException();
+      }
+    }
+
+    /// Ask Permission Again and Store Status
+    if (permission == LocationPermission.deniedForever) {
+      throw AppLocationPermissionForeverDeniedException();
+    }
+  }
 
   /// Get current device location
   void getCurrentLocation() {}
