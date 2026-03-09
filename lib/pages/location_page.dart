5287ca5b57ffbff5708a27181d5c04593e2fe394
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-09 15:47:30 +0530

    location_page.dart: if(){} else{} add inside submit().

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -124,16 +124,26 @@ class LocationPageState extends State<LocationPage> {
 
   /// Submit Button
   Future<void> submit() async {
+    /// NetworkService object
     final NetworkService _networkService = NetworkService();
+
+    /// Condition to pass userInput
     String q = searchController.text.isNotEmpty
         ? searchController.text
         : "$_lat,$_long";
+
+    /// Get object of weatherModel with data
     final weatherModel = await _networkService.getForecastData(q: q);
 
-    /// Return "weatherModel" to HomePage
-    Navigator.pop(context, weatherModel);
+    /// Check any error from WeatherModel if not then return to homePage()
+    if (weatherModel?.error != null) {
+      _showError("Server error: ${weatherModel?.error?.message}");
+    } else {
+      Navigator.pop(context, weatherModel);
+    }
   }
 
+  /// Show Snack bar
   void _showError(String message) {
     ScaffoldMessenger.of(
       context,
