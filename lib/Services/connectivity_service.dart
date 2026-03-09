28f4b5e926b68f8ab0c9cc5643992978abeb6a65
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-09 19:34:45 +0530

    connectivity_service.dart: documentation add.

--- lib/Services/connectivity_service.dart
+++ lib/Services/connectivity_service.dart
@@ -1,8 +1,10 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
+  /// Get object
   final Connectivity _connectivity = Connectivity();
 
+  /// Get internet connectivity result
   Future<bool> checkInternetConnectivity() async {
     final List<ConnectivityResult> result = await _connectivity
         .checkConnectivity();
