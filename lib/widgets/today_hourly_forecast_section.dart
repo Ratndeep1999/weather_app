2110f59fda8ae89e367759e45851fc73b747e73e
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-09 14:50:56 +0530

    hourly_data_utils.dart: one now parameter remove from getHourlyData().

--- lib/widgets/today_hourly_forecast_section.dart
+++ lib/widgets/today_hourly_forecast_section.dart
@@ -41,11 +41,7 @@ class TodayHourlyForecastSection extends StatelessWidget {
               separatorBuilder: (_, index) => SizedBox(width: width * 0.032),
               itemBuilder: (BuildContext context, int index) {
                 /// Get Specific Hour Item Data
-                final data = HourlyDataUtils.getHourlyData(
-                  weatherModel,
-                  index,
-                  now,
-                );
+                final data = HourlyDataUtils.getHourlyData(weatherModel, index);
 
                 /// Hourly Weather Report Item
                 return HourlyWeatherReportWidget(
