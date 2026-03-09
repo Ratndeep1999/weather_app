130d5e8344bae84b7792d3ff015b325c8d70c3c5
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-09 14:49:00 +0530

    hourly_data_utils.dart: DateTime now parameter remove from getHourlyData() and final now = DateTime.now(); add.

--- lib/utils/hourly_data_utils.dart
+++ lib/utils/hourly_data_utils.dart
@@ -6,12 +6,15 @@ class HourlyDataUtils {
 
   /// get Hourly items data
   static ({String temp, String icon, bool isCurrent, String time})
-  getHourlyData(weatherModel, int index, DateTime now) {
+  getHourlyData(weatherModel, int index) {
     /// Get Hourly data
     final hourlyData = weatherModel?.forecast?.forecastday?[0].hour?[index];
 
     /// Get hours
     final apiHour = hourlyData?.time?.hour ?? 0;
+
+    /// Get device time
+    final now = DateTime.now();
 
     /// Match api and local device hours
     final isCurrent = (apiHour == now.hour);
