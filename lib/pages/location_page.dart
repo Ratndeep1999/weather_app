2de88faa59e93188f7dacb7730173d1199d21dd9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-11 20:04:36 +0530

    location_page.dart: isNight: widget.isNight, height: widget.height, and width: widget.width, pass through GoogleMapPage().

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -172,6 +172,15 @@ class LocationPageState extends State<LocationPage> {
 
   /// Open Map Page
   void openMap() {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => GoogleMapPage()));
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => GoogleMapPage(
+          isNight: widget.isNight,
+          height: widget.height,
+          width: widget.width,
+        ),
+      ),
+    );
   }
 }
